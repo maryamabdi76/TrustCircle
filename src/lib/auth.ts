b2d08290be3d2c617f1ecb29207cfe6f8da6a50a
Maryam Abdi <maryamabdi9776@gmail.com>
2025-02-17 15:45:26 +0330

    delete

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -35,9 +35,6 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: '/auth/signin',
-  },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
