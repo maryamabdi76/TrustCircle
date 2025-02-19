4c8a93f9a9fecbe83f69bfca72e892a4d38d45ab
Maryam Abdi <maryamabdi9776@gmail.com>
2025-02-19 14:44:17 +0330

    fix navbar in mobile and login mode

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,36 +1,45 @@
 'use client';
 
-import { PATHS } from '@/constants/PATHS';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
-import { ThemeToggleButton } from './ThemeToggleButton';
-import { MobileMenu } from './MobileMenu';
-import { useTranslations } from 'next-intl';
-import { LanguageToggleButton } from './LanguageToggleButton';
-import { Logo } from '@/components/icons/Logo';
+import { LayoutDashboard, LogOut, User } from 'lucide-react';
+import { signOut, useSession } from 'next-auth/react';
+import { useLocale, useTranslations } from 'next-intl';
 import { useTheme } from 'next-themes';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useSession, signOut } from 'next-auth/react';
+import { Logo } from '@/components/icons/Logo';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { User, LogOut } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
-  DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { Skeleton } from '@/components/ui/skeleton';
+import { PATHS } from '@/constants/PATHS';
+import { MobileMenu } from './MobileMenu';
+import { ToggleLanguageIcon } from './ToggleLanguage';
+import { ToggleThemeIcon } from './ToggleTheme';
+import { setCookie } from 'cookies-next';
 
 export const Navbar = () => {
   const t = useTranslations('Navbar');
   const tAuth = useTranslations('Auth');
-  const { theme } = useTheme();
   const router = useRouter();
-  const { data: session } = useSession();
+  const { data: session, status: sessionStatus } = useSession();
+  const { theme, setTheme } = useTheme();
+  const locale = useLocale();
+
+  const toggleLanguage = () => {
+    const newLocale = locale === 'en' ? 'fa' : 'en';
+    setCookie('NEXT_LOCALE', newLocale, { path: '/' });
+    window.location.reload();
+  };
 
   return (
     <nav className="fixed top-0 left-0 right-0 bg-white dark:bg-gray-900 shadow-md z-50">
       <div className="mx-auto flex items-center justify-between py-4 px-6">
-        {/* Logo */}
         <Link href={PATHS.LANDING.ROOT} className="flex items-center gap-3">
           <Logo
             width={40}
@@ -42,19 +51,13 @@ export const Navbar = () => {
           </span>
         </Link>
 
-        {/* Buttons */}
-        <div className="flex items-center gap-4">
-          <Button variant="default" asChild className="hidden md:inline-block">
+        <div className="flex items-center gap-2">
+          <Button variant="default" asChild>
             <Link href={PATHS.BUSINESSES.ROOT}>{t('businesses')}</Link>
           </Button>
-
-          <LanguageToggleButton />
-
-          <ThemeToggleButton />
-
-          <MobileMenu />
-
-          {session ? (
+          {sessionStatus === 'loading' ? (
+            <Skeleton className="size-10 rounded-full" />
+          ) : session ? (
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button
@@ -77,16 +80,54 @@ export const Navbar = () => {
                   onClick={() => router.push(PATHS.PROFILE.ROOT)}
                 >
                   {t('profile')}
+                  <DropdownMenuShortcut>
+                    <LayoutDashboard size={15} />
+                  </DropdownMenuShortcut>
                 </DropdownMenuItem>
+
+                <DropdownMenuItem onClick={toggleLanguage}>
+                  {t('language')}
+                  <DropdownMenuShortcut>
+                    {locale === 'en' ? 'EN' : 'FA'}
+                  </DropdownMenuShortcut>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem
+                  onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+                >
+                  {t('theme')}
+                  <DropdownMenuShortcut>
+                    <ToggleThemeIcon />
+                  </DropdownMenuShortcut>
+                </DropdownMenuItem>
+
                 <DropdownMenuItem onClick={() => signOut()}>
-                  <LogOut className="mr-2 h-4 w-4" />
-                  <span>{tAuth('signOut')}</span>
+                  {tAuth('signOut')}
+                  <DropdownMenuShortcut>
+                    <LogOut size={15} />
+                  </DropdownMenuShortcut>
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
           ) : (
             <>
-              <Button variant="ghost" asChild>
+              <MobileMenu />
+              <Button
+                variant="outline"
+                className="hidden md:flex w-full cursor-pointer px-2"
+                onClick={toggleLanguage}
+              >
+                <ToggleLanguageIcon />
+                <span className="text-xs">{locale === 'en' ? 'EN' : 'FA'}</span>
+              </Button>
+              <Button
+                variant="outline"
+                className="hidden md:flex w-full cursor-pointer px-2"
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+              >
+                <ToggleThemeIcon />
+              </Button>
+              <Button className="hidden md:flex" variant="outline" asChild>
                 <Link href={PATHS.SIGNIN.ROOT}>{t('loginRegister')}</Link>
               </Button>
             </>
